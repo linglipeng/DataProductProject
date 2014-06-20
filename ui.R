d56963b234d3a76c19840e0e8db8a8e46270dd5f
linglipeng <linglipeng@yahoo.com>
2014-06-20 01:56:00 -0400

    updated

--- ui.R
+++ ui.R
@@ -4,9 +4,9 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
     numericInput('wt', 'Input the Car Weight (lb/1000):', 3, min = 1, max = 6, step = 0.5),
     radioButtons("cyl", "Input the number of cylinders:",
-                   c("Value 1" = "4",
-                     "Value 2" = "6",
-                     "Value 3" = "8"))
+                   c("4" = "4",
+                     "6" = "6",
+                     "8" = "8"))
      
   ),
   mainPanel(       
