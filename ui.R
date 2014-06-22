71c96e59c9d83068a6b7703ea6d94ecb713f0427
linglipeng <linglipeng@yahoo.com>
2014-06-22 10:17:59 -0400

    updated

--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 shinyUI(pageWithSidebar(
+  
   headerPanel("Predicting Car Fuel Efficiency (Mileage Per Gallon)"),
   sidebarPanel(
     numericInput('wt', 'Input the Car Weight (lb/1000):', 3, min = 1, max = 6, step = 0.5),
@@ -9,8 +10,9 @@ shinyUI(pageWithSidebar(
                      "8" = "8"))
      
   ),
-  mainPanel(       
-        h4('Based on your input, the predicted mpg value is:'),
+  mainPanel(  
+        h4('Instruction: You need to input the car weight (Min - 1, Max - 6 in lb/1000 unit) and the number of cylinders to predict MPG value. Use left panel inpt boxes to input.'),     
+        h4('Based on your input, the predicted MPG value is:'),
         verbatimTextOutput("ompg")
   )
 
